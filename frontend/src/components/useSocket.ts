467b1ad0ed3d3dd269722d13da2fcd0f5583a863
slink7 <slink794@gmail.com>
2026-04-13 16:31:57 +0200

    Removed hardcoded url

--- frontend/src/components/useSocket.ts
+++ frontend/src/components/useSocket.ts
@@ -2,7 +2,7 @@
 import { CONFIG } from "../config.ts"
 
 export function createSocket(onMessage: (msg: any) => void) {
-	const socket = new WebSocket("ws://10.12.1.11:3000");//CONFIG.WS_URL);
+	const socket = new WebSocket(CONFIG.WS_URL);
 
 	socket.onmessage = (event) => {
 		const data = JSON.parse(event.data);
