267f329585443b9081b1ce2eddc32a401742e3ed
basicjonjon <ledouaron.jonatan.sn@gmail.com>
2026-04-26 19:00:38 +0200

    add:listen server port 3001
    	modifié :         api/node_modules/.package-lock.json
    	modifié :         api/package-lock.json
    	modifié :         api/package.json
    	modifié :         api/src/index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import express from "express";
 import { Client, Pool } from "pg";
 import { CONFIG } from "./config.js";
@@ -58,6 +59,32 @@ app.listen(port, serverStart);
 //     console.log("test is called ");
 //     res.send(test);
 // });
+=======
+import express from "express"
+import { CONFIG } from "./config.js"
+
+const app = express();
+const port = CONFIG.PORT;
+
+function serverStart()
+{
+    console.log(`Le serveur est en cours d'exécution sur http://localhost:${port}` );
+}
+
+function callbackGet()
+{
+    console.log("une fonction geta ete utilisee");
+}
+
+app.get("/test", async (req, res) => {
+    callbackGet();
+    console.log("test is called");
+    res.send("Route /test appelée");
+});
+app.listen(port, serverStart);
+
+
+>>>>>>> 3e6fdc1 (add:listen server port 3001)
 // // const express = require('express');
 // // const cors =require('cors');
 // // const app = express();
