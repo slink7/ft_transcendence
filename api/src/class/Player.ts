717f26d0671c14dedea8f830b23054bb862b5bb3
basicjonjon <ledouaron.jonatan.sn@gmail.com>
2026-04-27 22:58:26 +0200

    add: playerlist function
    	modifié :         api/node_modules/.package-lock.json
    	modifié :         api/src/class/Player.ts

--- api/src/class/Player.ts
+++ api/src/class/Player.ts
@@ -1,7 +1,4 @@
 import { Theme } from "./Theme.js"
-import { Play } from "./Play.js"
-
-
 
 export class Player {
     id_player:number;
@@ -9,15 +6,12 @@ export class Player {
     email:string;
     color_pseudo:string;
     theme:Theme;
-    play:Play[];
 
-
-    constructor(id_player: number, username: string, email: string, color_pseudo: string, theme:Theme, play:Play[]) {
+    constructor(id_player: number, username: string, email: string, color_pseudo: string, theme:Theme) {
         this.id_player = id_player;
         this.username=username;
         this.email=email;
         this.color_pseudo=color_pseudo;
-        this.theme=theme;
-        this.play=play;
+        this.theme = theme;
     };
 }
