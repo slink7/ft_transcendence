561e5ad5a990dd2a5d6e5b440debd504770206b5
slink7 <slink794@gmail.com>
2026-04-30 14:12:06 +0200

    Added working game

--- backend/src/game/GameManager.ts
+++ backend/src/game/GameManager.ts
@@ -10,16 +10,14 @@ type Player = {
 export class GameManager {
 	private players: Map<string, Player> = new Map();
 
-	addPlayer(ws: WebSocket): string {
-		const id = crypto.randomUUID();
-
-		this.players.set(id, {
-			id,
+	addPlayer(clientID: string, ws: WebSocket): string {
+		this.players.set(clientID, {
+			id: clientID,
 			ws,
 			game: new Game()
 		});
 
-		return id;
+		return clientID;
 	}
 
 	removePlayer(id: string) {
@@ -35,20 +33,29 @@ export class GameManager {
 		game.applyInput(msg);
 	}
 
-	update() {
-		this.players.forEach((player) => {
-			player.game.update();
-		});
+	update(UUID: string) {
+		this.players.get(UUID)?.game.update();
+		// this.players.forEach((player) => {
+		// 	player.game.update();
+		// });
 	}
 
-	broadcast() {
-		this.players.forEach((player) => {
-			const state = player.game.getState();
-
-			player.ws.send(JSON.stringify({
-				type: "STATE",
-				state
-			}));
-		});
+	broadcast(UUID: string) {
+		const player = this.players.get(UUID);
+		if (!player)
+			return ;
+		console.log("Sending to ", player.ws);
+		player.ws.send(JSON.stringify({
+			type: "STATE",
+			state: player.game.getState()
+		}));
+		// this.players.forEach((player) => {
+		// 	const state = player.game.getState();
+		//
+		// 	player.ws.send(JSON.stringify({
+		// 		type: "STATE",
+		// 		state
+		// 	}));
+		// });
 	}
 }
