9a4d76f0176cd48f104c0e3271a4ce589e22c5d7
slink7 <slink794@gmail.com>
2026-05-01 19:44:22 +0200

    fix: room generator

--- frontend/src/components/home/roomsetter/RoomGenerator.tsx
+++ frontend/src/components/home/roomsetter/RoomGenerator.tsx
@@ -1,10 +1,12 @@
-import { send } from "../../../scripts/socket.ts";
+import { subscribe, send } from "../../../scripts/socket.ts";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react"
 
 export default function RoomGenerator() {
 	const { t } = useTranslation();
 	const navigate = useNavigate();
+	const [roomID, setRoomID] = useState<string | null>(null);
 
 	function navToRoom(ID: string) {
 		navigate(`/room/${ID}`);
@@ -20,12 +22,21 @@ export default function RoomGenerator() {
 		navToRoom(ID);
 	};
 
+	useEffect(() => {
+		return (subscribe((msg: ServerMessage) => {
+			setRoomID(msg.roomID);
+			console.log("SetRoomID");
+		}, "CREATED_ROOM"));
+	}, []);
+
+	useEffect(() => {
+		if (roomID)
+			navToRoom(roomID);
+	}, [roomID]);
+
 	return (
-		<form className="flex items-center border-2 border-black-800 rounded" action={joinRoom}>
-			<input className="py-2 px-4 bg-white" name="roomID"/>
-			<button className="bg-yellow-500 hover:bg-yellow-600 text-yellow-50 py-2 px-4" onClick={createRoom}>
-				{t('home.create')}
-			</button>
-		</form>
+		<button type="submit" className="bg-yellow-500 hover:bg-yellow-600 text-yellow-50 py-2 px-4" onClick={createRoom}>
+			{t('home.create')}
+		</button>
 	)
 }
