9f4e479f2507a5062018e9da72534029fd68e504
basicjonjon <ledouaron.jonatan.sn@gmail.com>
2026-05-02 23:41:16 +0200

    add jwt auth

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,3 +1,4 @@
 export const CONFIG = {
 	PORT: process.env.API_PORT || 3001,
+	
 };
--- api/src/routes/Player.routes.ts
+++ api/src/routes/Player.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { addPlayer, getAllPlayer, getPlayer, loginPlayer} from "../controllers/Player.controller.js";
- 
+ import { jwtAuth } from "../security/jwt.verif.js";
 export const playerRouter = Router();
 
-playerRouter.get("/playerlist", getAllPlayer);
+playerRouter.get("/playerlist",jwtAuth, getAllPlayer);
 playerRouter.get("/username/:username", async (req, res) => { getPlayer(req, res, "username");});
 playerRouter.get("/id_player/:id_player", async (req, res) => { getPlayer(req, res, "id_player");});
 playerRouter.post("/login", loginPlayer);
