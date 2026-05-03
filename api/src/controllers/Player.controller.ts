103fabd0b9b95659c7200df2efbf91e72c95350f
basicjonjon <ledouaron.jonatan.sn@gmail.com>
2026-05-03 18:39:43 +0200

    jwt work

--- api/src/controllers/Player.controller.ts
+++ api/src/controllers/Player.controller.ts
@@ -53,17 +53,6 @@ export async function getPlayer(req: Request, res: Response, field: "id_player" 
 export async function loginPlayer(req: Request, res: Response) {
     const { email, pwd } = req.body
     try {
-
-        const value = [email, pwd];
-        if (!email || !pwd) {
-            return res.status(400).send({
-                error: "Missing required fields"
-            });
-        }
-        console.log(`try get Player ${email}`);
-    const { email, pwd } = req.body
-    try {
-
         const value = [email, pwd];
         if (!email || !pwd) {
             return res.status(400).send({
@@ -75,28 +64,9 @@ export async function loginPlayer(req: Request, res: Response) {
         if (playerResult.rowCount == 0) {
             res.status(404).send({
                 error: `player ${email} not found`
-                error: `player ${email} not found`
             });
             return;
         }
-        const jwtSecret = process.env.JWT_SECRET;
-        if (!jwtSecret) {
-            throw new Error("JWT_SECRET is missing from environment variables");
-        }
-        const player: Player = parsePlayer(playerResult.rows[0]);
-        const token = jwt.sign(
-            {
-                id: player.id_player,
-                username: player.username,
-                id_theme: player.id_theme
-            },
-            jwtSecret,
-            {
-                expiresIn: "1h"
-            }
-        );
-        res.status(200).send(token);
-        console.log(`player ${email} were successfully sent`);
         const jwtSecret = process.env.JWT_SECRET;
         if (!jwtSecret) {
             throw new Error("JWT_SECRET is missing from environment variables");
@@ -143,7 +113,6 @@ export async function addPlayer(req: Request, res: Response) {
         }
         res.status(201).send(`player ${username} succesfully created`);
     }
-    catch (err: any) {
     catch (err: any) {
 
         if (err.code === "23505") {
